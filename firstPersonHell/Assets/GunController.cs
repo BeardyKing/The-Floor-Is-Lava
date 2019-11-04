55f5df5dbce5ccb64688031844a22cfcebd240b2
BeardyKing <khopking@gmail.com>
2019-11-04 22:16:00 +0000

    Init Commit
    Current state of project

---
+++ firstPersonHell/Assets/GunController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GunController : MonoBehaviour{
+    [SerializeField] private GameObject hitObj;
+    private Camera cam;
+    private List<GameObject> hitList = new List<GameObject>();
+    private GameObject[] shootPoints = new GameObject[2];
+    private GameObject[] shotObjects = new GameObject[2];
+    
+    void Start(){
+        cam = FindObjectOfType<Camera>();
+    }
+
+    private bool doOnce;
+    void Update(){
+        if (Input.GetButtonDown("Fire1")){
+            doOnce = true;
+            RaycastHit hit;
+            var ray = cam.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2,0));
+
+            if (Physics.Raycast(ray, out hit)){
+                if (!shootPoints[0]){
+                    shootPoints[0] = Instantiate(hitObj);
+                }
+
+                shootPoints[0].name = "A";
+                shootPoints[0].transform.position = hit.point;
+                shotObjects[0] = hit.transform.gameObject;
+            }
+        }
+
+        if (Input.GetButtonDown("Fire2")){
+            doOnce = true;
+            RaycastHit hit;
+            var ray = cam.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2,0));
+
+            if (Physics.Raycast(ray, out hit)){
+
+                if (Physics.Raycast(ray, out hit)){
+                    if (!shootPoints[1]){
+                        shootPoints[1] = Instantiate(hitObj);
+                    }
+
+                    shootPoints[1].name = "B";
+                    shootPoints[1].transform.position = hit.point;
+                    shotObjects[1] = hit.transform.gameObject;
+                }
+            }
+        }
+
+        if (shotObjects[0] != shotObjects[1]){
+            if (doOnce){
+                doOnce = false;
+                Vector3 dir0,dir1;
+                dir0 = (shotObjects[1].transform.position - shotObjects[0].transform.position) ;
+                dir1 = (shotObjects[0].transform.position - shotObjects[1].transform.position) ;
+
+                shotObjects[0].GetComponent<Rigidbody>().velocity = dir0.normalized * 20;
+                shotObjects[1].GetComponent<Rigidbody>().velocity = dir1.normalized * 20;
+                
+                shotObjects = new GameObject[2];
+                
+                Destroy(shootPoints[0]);
+                Destroy(shootPoints[1]);
+            }
+        }
+    }
+}
---
+++ firstPersonHell/Assets/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    [SerializeField] private bool hideMouse;
+    private Camera cam;
+    private Rigidbody rb;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private Vector3 rotationEuler;
+    
+    
+    private void Start(){
+        Cursor.visible = hideMouse;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        cam = FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update(){
+        RotatePlayer();
+        PlayerMovement();
+    }
+
+    private float maxHeight,minHeight;
+    private void RotatePlayer(){
+        rotationEuler += new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"),0);
+        
+        if (rotationEuler.x > 60){
+            rotationEuler = new Vector3(60,rotationEuler.y,0);
+        }
+        if (rotationEuler.x < -60){
+            rotationEuler = new Vector3(-60,rotationEuler.y,0);
+        }
+
+        transform.eulerAngles = new Vector3(0,rotationEuler.y,0);
+        cam.transform.localEulerAngles = new Vector3(rotationEuler.x,0 , 0);
+    }
+    Vector3 inDir;
+
+    private float timer;
+    private void PlayerMovement(){
+        if (Input.GetButtonDown("Jump")){
+            rb.AddForce(Vector3.up * 200);
+            timer = .2f;
+        }
+        inDir = Vector3.zero;
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            if (direction.z != 0){
+                rb.AddForce(transform.forward * direction.z * 4);
+            }
+            if (direction.x != 0){
+                rb.AddForce(transform.right * direction.x * 4);
+            }
+
+            if (direction.x == 0 && direction.z == 0){
+                    rb.velocity = new Vector3(rb.velocity.x / 128, rb.velocity.y, rb.velocity.z / 128);
+            }
+
+
+    }
+}
