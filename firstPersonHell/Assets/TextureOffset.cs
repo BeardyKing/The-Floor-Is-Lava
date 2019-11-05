8161cad9cc080d9010709a4bf81659b459402ead
BeardyKing <khopking@gmail.com>
2019-11-05 01:08:19 +0000

    bringOver

---
+++ firstPersonHell/Assets/TextureOffset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffset : MonoBehaviour
+{
+    Material mat;
+    // Start is called before the first frame update
+    void Start(){
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        mat.SetTextureOffset(1, new Vector2(Time.time / 32, Time.time / 32));
+    }
+}
